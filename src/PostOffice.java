863e3aae6cb41bcb34d9bdc5efbcfab872dbe868
StanchevaYoana <yoana.radoslavova@gmail.com>
2019-08-01 12:44:57 +0300

    Add task - Post office 90p .

--- src/PostOffice.java
+++ src/PostOffice.java
@@ -1,4 +1,3 @@
-import java.io.Console;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -7,20 +6,18 @@ import java.util.stream.Collectors;
 public class PostOffice {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        String regexCapitals = "([#$%*&]+)(?<capitals>[A-Z]+)\\1";
-        String regexLength = "(?<letter>[\\d]{2}):(?<length>[0-9][0-9])";
-
-        Pattern patternCapitals = Pattern.compile(regexCapitals);
-        Pattern patternLength = Pattern.compile(regexLength);
-
         String[] input = scanner.nextLine().split("\\|");
-        List<String> capitals = new ArrayList<>();
 
+        Pattern patternCapitals = Pattern.compile("([#$%*&]+)(?<capitals>[A-Z]+)\\1");
+        Pattern patternLength = Pattern.compile("(?<letter>[\\d]{2}):(?<length>[0-9][0-9])");
+
+        List<String> capitals = new ArrayList<>();
         Matcher matcherCapitals = patternCapitals.matcher(input[0]);
         if (matcherCapitals.find()) {
-            capitals = Arrays.stream(matcherCapitals.group("capitals").split("")).collect(Collectors.toList());
+            capitals = Arrays.stream(matcherCapitals.group("capitals")
+                    .split("")).collect(Collectors.toList());
         }
+
         Map<String, Integer> letterLength = new LinkedHashMap<>();
         capitals.forEach(e -> letterLength.putIfAbsent(e, 0));
 
@@ -36,12 +33,10 @@ public class PostOffice {
         }
 
         String[] words = input[2].split(" ");
-
-
-        for (String word : words){
+        for (String word : words) {
             String firstLetter = String.valueOf(word.charAt(0));
-            if (letterLength.containsKey(firstLetter)){
-                if(word.length() == letterLength.get(firstLetter)+1){
+            if (letterLength.containsKey(firstLetter)) {
+                if (word.length() == letterLength.get(firstLetter) + 1) {
                     System.out.println(word);
                 }
             }
